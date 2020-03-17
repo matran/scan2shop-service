449616d89bc8c96e579c79d09fb4f9f935f06f2f
henry cheruiyot <matranic@gmail.com>
2020-03-17 18:14:30 +0300

    change config

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://pfjvpvkxnewmbb:500f424b84de714cb65e0128880fa098577a5d0629c834f3e2407319455cf68e@ec2-18-210-51-239.compute-1.amazonaws.com:5432/ddgrf8kj6euol6"
+SQLALCHEMY_DATABASE_URI = "postgresql://yjmeixtqsoxpyv:da5156c6578aff52851cf14d749ddb9c6a3403f0e748afce6979181fe49dc794@ec2-18-233-137-77.compute-1.amazonaws.com:5432/ddkfu1dug68ind"
 #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:henry5765@localhost/flaskapp"
 #SQLALCHEMY_DATABASE_URI = "postgresql://postgres:henry5765@localhost/scan2shop"
 SECRET_KEY ="\xbfK\xcf\xbf\xb4 \x83\xfa\xd8W\xd1Ey\xba\xc4\xd0\x1e\xe5\xb7m\xef_\xe0\x1a"
@@ -14,6 +14,6 @@ MAIL_SERVER='smtp.gmail.com'
 MAIL_PORT=587  
 MAIL_USE_TLS=1
 MAIL_USERNAME='matranic@gmail.com'
-MAIL_PASSWORD='Divine*henry123'
+MAIL_PASSWORD='Divine*power123'
 UPLOAD_FOLDER='./static'
 AUTHY_API_KEY = '5NWHaP7ZVYQjp66bMpiM16DfXkeJCnw8'
